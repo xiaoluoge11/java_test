2776d297a1a5cd6fbc69cb10954ef6f6e8ae9c45
Your Name <you@example.com>
2017-12-02 11:45:24 +0800

    commit a

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -1,8 +1,21 @@
-public class HelloWorld {
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 
-	public static void main(String[] args) {
-		System.out.println("HelloWorld");
+@WebServlet("/hello")
+public class HelloWorld extends HttpServlet {
 
-	}
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.getWriter().println("Hello, World!");
+    }
 
-}
+    @Override
+    public void destroy() {
+        super.destroy();
+    }
+
+} 
